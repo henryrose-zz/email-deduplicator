7b9d688c4550c977c55e7ade58bb0e5da2b27b49
Henry Rose <henryr@porch.com>
2017-01-29 19:11:49 -0800

    use map instead of array.  much faster

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ var RandomEmailGenerator = require('./randomEmailGenerator');
 
 //var emails = ['cat@hat.com', 'dog@bog.net', 'bill@sill.ws', 'cat@hat.dog', 'al@pal.org']; 
 var now = Date.now(); 
-var emails = RandomEmailGenerator.generateWithDupes(50000); 
+var emails = RandomEmailGenerator.generateWithDupes(100000); 
 console.log('generating took ' + (Date.now() - now) + 'ms'); 
 console.log('dedupe these ' + emails.length + ' emails'); 
 
- 
-var startTime = Date.now();
-dedupe(emails, function (err, deduped) {
-    
-    var endTime = Date.now(); 
-    console.log('Deduped email count: ', deduped.length);
-    console.log('Deuping took ' + (endTime-startTime) + 'ms');
-})
+
+console.log('lets use an array first:')
+now = Date.now(); 
+var deduped = dedupe.arrayDedupe(emails);
+console.log('Deduped email count: ', deduped.length);
+console.log('Deuping took ' + (Date.now() - now) + 'ms');
+
+
+console.log('ok lets try using a javascript Object or map');
+now = Date.now(); 
+var deduped = dedupe.mapDedupe(emails);
+var endTime = Date.now(); 
+console.log('Deduped email count: ', deduped.length);
+console.log('Deuping took ' + (Date.now() - now) + 'ms');
+
 
 
 
--- dedupe.js
+++ dedupe.js
@@ -2,19 +2,39 @@
  * This module contains a function to dedupe an array of email addresses
  */
 
-module.exports = function(emails, callback){
+// This was my first attempt, it was slow!
+function iterativeDedupe(emails){
   var deduped = []; // place to put our deduped emails
   var err; 
   var email; 
-
   // iterate through list of emails
   for (var i = 0; i < emails.length; i++){
     email = emails[i]; 
-
+    
     // check for the presence of this email in our deduplicated array
-    if (deduped.indexOf(email) < 0){
+    if (deduped.indexOf(email) < 0){      
         deduped.push(email);
     }
   }
-  callback(err, deduped) 
+  return deduped;  
 };
+
+// This is much faster
+function mapDedupe(emails){
+  var email; 
+  var obj = {}; 
+  var out = []; 
+
+  for (var i = 0; i < emails.length; i++){
+    email = emails[i];
+    if (typeof(obj[email] === 'undefined')){
+      obj[email] = 1; 
+    }
+  }
+  return Object.keys(obj); 
+}
+
+module.exports = {
+  iterate : iterativeDedupe,
+  mapDedupe: mapDedupe
+}
